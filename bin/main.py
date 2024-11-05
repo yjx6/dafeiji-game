1d8cd37d193903815d7d3db95e637b115062e716
yangjianxin <1206869822@qq.com>
2024-11-05 20:34:33 +0800

    添加了退出按键

--- bin/main.py
+++ bin/main.py
@@ -146,7 +146,8 @@ def main():
 
         # 响应用户的操作
         for event in pygame.event.get():
-            if event.type == 12:  # 如果用户按下屏幕上的关闭按钮，触发QUIT事件，程序退出
+            if event.type == pygame.KEYDOWN:  # 如果用户按下屏幕上的关闭按钮，触发QUIT事件，程序退出
+               if event.type==pygame.K_ESCAPE: 
                 pygame.quit()
                 sys.exit()
 
